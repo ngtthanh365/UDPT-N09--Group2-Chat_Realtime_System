f12ee4a90d9e7fc6b50f24784678c810b4ebf9d1
DangDacTu <dagdtus@gmail.com>
2026-05-17 15:00:06 +0700

    signin & signup ui

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,13 +1,13 @@
-import { SigninForm } from "@/components/auth/signin-form";
+import { SignupForm } from "@/components/auth/signup-form";
 
-const SignInPage = () => {
+const SignUpPage = () => {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 absolute inset-0 z-0 bg-gradient-purple">
       <div className="w-full max-w-sm md:max-w-4xl">
-        <SigninForm />
+        <SignupForm />
       </div>
     </div>
   );
 };
 
-export default SignInPage;
+export default SignUpPage;
