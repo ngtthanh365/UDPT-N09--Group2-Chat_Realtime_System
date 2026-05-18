0f33bc39e999743a18c6f42b58c83e5256ee59e0
Nguyen Tuan Thanh <tuanthanh365bt@gmail.com>
2026-05-18 14:18:33 +0700

    setup distributed infrastructure and gateway service

---
+++ gateway-service/src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Gateway running on port 5000");
+});
