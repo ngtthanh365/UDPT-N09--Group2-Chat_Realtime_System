f34ed5b52f1974ea9c9cedfc11af448c1c94ea49
Nguyen Tuan Thanh <tuanthanh365bt@gmail.com>
2026-05-20 22:13:56 +0700

    feat: ket noi  websocket phan tan voi redis pub/sub

--- gateway-service/src/server.js
+++ gateway-service/src/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const { createAdapter } = require("@socket.io/redis-adapter");
+
 const cors = require("cors");
+
+const {
+    pubClient,
+    subClient,
+    connectRedis,
+} = require("./redis/redisClient");
 
 const app = express();
 
@@ -10,19 +18,45 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
+    cors: {
+        origin: "*",
+    },
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+async function startServer() {
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
+    await connectRedis();
 
-server.listen(5000, () => {
-  console.log("Gateway running on port 5000");
-});
+    io.adapter(createAdapter(pubClient, subClient));
+
+    io.on("connection", (socket) => {
+
+        console.log("User connected:", socket.id);
+
+        socket.on("send_message", (data) => {
+
+            console.log("Message received:", data);
+
+            io.emit("receive_message", data);
+
+        });
+
+        socket.on("disconnect", () => {
+
+            console.log("User disconnected:", socket.id);
+
+        });
+
+    });
+
+    const PORT = process.env.PORT || 5000;
+
+    server.listen(PORT, () => {
+
+        console.log(`Gateway running on port ${PORT}`);
+
+    });
+
+}
+
+startServer();
